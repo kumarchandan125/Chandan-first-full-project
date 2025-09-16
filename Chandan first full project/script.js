d806efed8c06d41a979d9f3d5bafe82bff5e5c68
kumarchandan125 <chandanpandey6048178@gmail.com>
2025-09-16 15:30:53 +0530

    Add files via upload

---
+++ Chandan first full project/script.js
@@ -0,0 +1,125 @@
+let cursor = document.querySelector("#cursor");
+let blur = document.querySelector("#cursor-blur");
+
+document.addEventListener("mousemove", (dets) => {
+  cursor.style.left = dets.x+30 + "px";
+  cursor.style.top = dets.y + "px";
+  blur.style.left = dets.x - 250 + "px";
+  blur.style.top = dets.y - 250 + "px";
+});
+
+var h4all = document.querySelectorAll("#nav h4");
+  h4all.forEach((elem)=>{
+    elem.addEventListener("mouseenter",()=>{
+        cursor.style.scale=3;
+        cursor.style.border="1px solid #fff"
+        cursor.style.backgroundColor="transparent"
+    })
+
+    elem.addEventListener("mouseleave",()=>{
+        cursor.style.scale=1;
+        cursor.style.border="0px solid #95C11E"
+        cursor.style.backgroundColor="#95C11E"
+    })
+  })
+
+gsap.to("#nav", {
+  backgroundColor: "#000",
+  height: "110px",
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#nav",
+    scroller: "body",
+    // markers:true,
+    start: "top -10%",
+    end: "top -11%",
+    scrub: 1,
+  },
+});
+
+gsap.to("#main", {
+  backgroundColor: "#000",
+  scrollTrigger: {
+    trigger: "#main",
+    scroller: "body",
+    // markers:true,
+    start: "top -25%",
+    end: "top -70%",
+    scrub: 2,
+  },
+});
+
+gsap.from("#about-us img,#about-us-in",{
+  y:90,
+  opacity:0,
+  duration:1,
+  scrollTrigger:{
+    trigger:"#about-us",
+    scroller:"body",
+    start:"top 70%",
+    end:"top 65%",
+    scrub:1,
+  },
+});
+
+gsap.from(".card",{
+  scale:0.8,
+  // opacity:0,
+  duration:1,
+  stagger:0.1,
+  scrollTrigger:{
+    trigger:".card",
+    scroller:"body",
+    start:"top 70%",
+    end:"top 65%",
+    scrub:1,
+  },
+});
+
+
+gsap.from("#colon1", {
+  y: -70,
+  x: -70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#colon2", {
+  y: 70,
+  x: 70,
+  scrollTrigger: {
+    trigger: "#colon1",
+    scroller: "body",
+    // markers:true,
+    start: "top 55%",
+    end: "top 45%",
+    scrub: 4,
+  },
+});
+gsap.from("#page4 h1", {
+  y: 50,
+  scrollTrigger: {
+    trigger: "#page4 h1",
+    scroller: "body",
+    // markers:true,
+    start: "top 75%",
+    end: "top 70%",
+    scrub: 3,
+  },
+});
+
+
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
+
+
+
